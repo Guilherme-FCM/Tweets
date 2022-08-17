5981cc5025ef5207a77f780278793f43619db757
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-16 23:46:00 -0300

    Add validation on create users method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,18 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
+  Res,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    const credentials = [];
+    if (!createUserDto.email && !createUserDto.telephone)
+      return res.json({ message: 'Email or telephone is required.' });
+
+    if (createUserDto.email) credentials.push({ email: createUserDto.email });
+
+    if (createUserDto.telephone)
+      credentials.push({ telephone: createUserDto.telephone });
+
+    const findedUsers = await this.usersService.countByCredentials(credentials);
+    if (findedUsers > 0)
+      return res.json({
+        message: 'This email or telephone is vinculated to a account.',
+      });
+
+    const createdUser = await this.usersService.create(createUserDto);
+    return res.status(HttpStatus.CREATED).json(createdUser);
   }
 
   @Get()
