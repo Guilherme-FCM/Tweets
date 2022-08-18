3bcedc7b94658f3f0f38abccedfdcb5fc48fd154
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-18 16:35:42 -0300

    Validate create-user-dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,18 @@
+import { IsEmail, IsNotEmpty, IsNumberString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNumberString()
   telephone?: string;
+
+  @IsEmail()
   email?: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   birthDate: string;
 }
