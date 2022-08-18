cc096724869e21d7177a7d80cff9302c05844cfc
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-18 17:12:18 -0300

    Validate login-dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,19 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+} from 'class-validator';
+
 export class LoginDto {
+  @IsEmail()
+  @IsOptional()
   email?: string;
+
+  @IsNumberString()
+  @IsOptional()
   telephone?: string;
+
+  @IsNotEmpty()
   password: string;
 }
