1777e4f8c6dbe596d8e029f1d0e84028c5f923a7
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-18 17:07:01 -0300

    Remove content on update-tweet-dto

--- src/tweets/dto/update-tweet.dto.ts
+++ src/tweets/dto/update-tweet.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNotEmpty } from 'class-validator';
 import { CreateTweetDto } from './create-tweet.dto';
 
 export class UpdateTweetDto extends PartialType(CreateTweetDto) {}
