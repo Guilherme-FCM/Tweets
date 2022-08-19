86b2fb4e3866fb12930c61071919fc9fc5545dc9
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-19 15:52:44 -0300

    Create login method on auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.usersService.findByEmail(email);
+    console.log(user);
 
     if (!user) return null;
     const isAutenticated = await bcrypt.compare(password, user.password);
 
     if (!isAutenticated) return null;
     return user;
+  }
+
+  login(user) {
+    const payload = { username: user.username, sub: user.userId };
+    return this.jwtService.sign(payload);
   }
 }
