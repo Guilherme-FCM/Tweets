84e0d47108bb59b8164cd361e8ff24640258729e
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-19 16:55:09 -0300

    Create method to get user tweets

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Op } from 'sequelize';
 import { WhereOptions } from 'sequelize';
+import { Tweet } from 'src/tweets/entities/tweet.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -26,6 +27,14 @@ export class UsersService {
   findOne(id: number) {
     return this.userModel.findOne({
       where: { id },
+      attributes: ['id', 'email', 'telephone', 'name', 'birthDate'],
+    });
+  }
+
+  findTweets(id: number) {
+    return this.userModel.findOne({
+      where: { id },
+      include: [Tweet],
       attributes: ['id', 'email', 'telephone', 'name', 'birthDate'],
     });
   }
