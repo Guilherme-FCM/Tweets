850f724a9f0be827889bf5a0ace5666942c04d42
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-19 09:33:44 -0300

    Validate user login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,21 +2,39 @@ import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
+  @Post('/login')
   async login(@Body() loginDto: LoginDto, @Res() res: Response) {
     if (!loginDto.email && !loginDto.telephone)
       return res
         .status(HttpStatus.BAD_REQUEST)
         .json({ message: 'Email or telephone is required.' });
-    else if (!loginDto.email) delete loginDto.email;
-    else delete loginDto.telephone;
 
-    const autenticatedUser = await this.authService.login(loginDto);
-    return res.json(autenticatedUser);
+    const user = loginDto.email
+      ? await this.authService.findUserByEmail(loginDto.email)
+      : await this.authService.findUserByTelephone(loginDto.telephone);
+
+    if (!user)
+      return res
+        .status(HttpStatus.NOT_FOUND)
+        .json({ message: 'User not found.' });
+
+    const isAutenticated = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!isAutenticated)
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'Invalid password.' });
+
+    const token = 'token';
+    return res.status(HttpStatus.OK).json({ user, token });
   }
 }
