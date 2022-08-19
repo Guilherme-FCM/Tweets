43ed2d7d8d787f4bcacbfc236d10391da08338da
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-19 16:53:53 -0300

    Add tweets on user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
+import { Tweet } from 'src/tweets/entities/tweet.entity';
 
 @Table({ tableName: 'users' })
 export class User extends Model {
@@ -16,4 +17,7 @@ export class User extends Model {
 
   @Column(DataType.DATE)
   birthDate: string;
+
+  @HasMany(() => Tweet)
+  tweets: Tweet[];
 }
